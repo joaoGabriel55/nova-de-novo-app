2794a10922c4c571667cdacdba157ff41283cac2
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2020-06-07 11:15:19 -0300

    Added date interval filter

--- back-end/src/controllers/ServiceOrderController.js
+++ back-end/src/controllers/ServiceOrderController.js
@@ -1,5 +1,6 @@
 import models, { sequelize } from '../models';
 import { Exception } from '../exceptions/responseException'
+const { Op } = require("sequelize");
 
 const validateDate = (date, dataCompare) => {
     console.log(date, dataCompare)
@@ -9,13 +10,13 @@ const validateDate = (date, dataCompare) => {
     return true
 }
 
+const validateStatus = (status) => {
+    if (!(status.toUpperCase() === 'FINISHED') && !(status.toUpperCase() === 'PENDING'))
+        return false
+    return true
+}
+
 const validateServiceOrder = (res, service) => {
-
-    if (!service.entryDate || service.entryDate === '')
-        return Exception(res, 400, 'Entry date is required')
-
-    if (!validateDate(new Date(service.entryDate), new Date()))
-        return Exception(res, 400, 'Entry date is invalid')
 
     if (!service.deliveryDate || service.deliveryDate === '')
         return Exception(res, 400, 'Delivery date is required')
@@ -32,7 +33,7 @@ const validateServiceOrder = (res, service) => {
 
     if (!service.status || service.status === '')
         return Exception(res, 400, 'Status is required')
-    if (!(service.status.toUpperCase() === 'FINISHED') && !(service.status.toUpperCase() === 'PENDING'))
+    if (!validateStatus(service.status))
         return Exception(res, 400, 'Status must be \'FINISHED\' or \'PENDING\'')
 
     if (!service.customerId || service.customerId === '')
@@ -44,6 +45,42 @@ const validateServiceOrder = (res, service) => {
         return Exception(res, 400, 'Total price is required or invalid')
 
     return null
+}
+
+const getSelector = (orderByPrice, limit, offset, status, dateField, startDate, endDate) => {
+    let selector = {
+        order: orderByPrice ? [['totalPrice', orderByPrice.toUpperCase()]] : [],
+        include: ['services'],
+        limit: limit,
+        offset: offset,
+    }
+
+    const validDateFilter = (startDate && endDate && dateField)
+
+    if (status || validDateFilter)
+        selector['where'] = { [Op.and]: [] }
+
+    if (status) {
+        if (validateStatus(status)) {
+            selector.where[Op.and].push({ status: status })
+        } else {
+            return Exception(res, 400, 'Wrong status!')
+        }
+    }
+
+    if (validDateFilter && new Date(startDate).getTime() < new Date(endDate).getTime()) {
+        selector.where[Op.and].push({
+            [dateField]: {
+                [Op.between]: [new Date(startDate), new Date(endDate)]
+            }
+        })
+    } else if (validDateFilter && new Date(startDate).getTime() === new Date(endDate).getTime()) {
+        selector.where[Op.and].push({
+            [dateField]: new Date(startDate)
+        })
+    }
+
+    return selector
 }
 
 const serviceOrder = async (data) => {
@@ -66,14 +103,16 @@ const index = async (req, res) => {
         const limit = req.query.limit
         const offset = req.query.offset
 
-        let serviceOrders = await models.ServiceOrder.findAndCountAll(
-            {
-                order: orderByPrice ? [['totalPrice', orderByPrice.toUpperCase()]] : [],
-                include: ['services'],
-                limit: limit,
-                offset: offset,
-            }
-        )
+        const status = req.query.status
+
+        const dateField = req.query.dateField
+        const startDate = req.query.startDate
+        const endDate = req.query.endDate
+
+        const selector = getSelector(orderByPrice, limit, offset, status, dateField, startDate, endDate)
+        console.log(selector)
+
+        let serviceOrders = await models.ServiceOrder.findAndCountAll(selector)
 
         for (const element of serviceOrders.rows)
             await serviceOrder(element.dataValues)
@@ -86,22 +125,26 @@ const index = async (req, res) => {
 
 const findByCustomerId = async (req, res) => {
     const customerId = req.params.id
-    const selector = { customerId: customerId }
 
     try {
         const orderByPrice = req.query.orderByPrice
+
         const limit = req.query.limit
         const offset = req.query.offset
 
-        let serviceOrders = await models.ServiceOrder.findAndCountAll(
-            {
-                order: orderByPrice ? [['totalPrice', orderByPrice.toUpperCase()]] : [],
-                where: selector,
-                include: ['services'],
-                limit: limit,
-                offset: offset,
-            }
-        )
+        const status = req.query.status
+
+        const dateField = req.query.dateField
+        const startDate = req.query.startDate
+        const endDate = req.query.endDate
+
+        let selector = getSelector(orderByPrice, limit, offset, status, dateField, startDate, endDate)
+        selector.where[Op.and].push({
+            ['customerId']: customerId
+        })
+        console.log(selector)
+
+        let serviceOrders = await models.ServiceOrder.findAndCountAll(selector)
 
         for (const element of serviceOrders.rows)
             await serviceOrder(element.dataValues)
@@ -114,22 +157,26 @@ const findByCustomerId = async (req, res) => {
 
 const findByDressMakerId = async (req, res) => {
     const dressmakerId = req.params.id
-    const selector = { dressmakerId: dressmakerId }
 
     try {
         const orderByPrice = req.query.orderByPrice
+
         const limit = req.query.limit
         const offset = req.query.offset
 
-        let serviceOrders = await models.ServiceOrder.findAndCountAll(
-            {
-                order: orderByPrice ? [['totalPrice', orderByPrice.toUpperCase()]] : [],
-                where: selector,
-                include: ['services'],
-                limit: limit,
-                offset: offset,
-            }
-        )
+        const status = req.query.status
+
+        const dateField = req.query.dateField
+        const startDate = req.query.startDate
+        const endDate = req.query.endDate
+
+        let selector = getSelector(orderByPrice, limit, offset, status, dateField, startDate, endDate)
+        selector.where[Op.and].push({
+            ['dressmakerId']: dressmakerId
+        })
+        console.log(selector)
+
+        let serviceOrders = await models.ServiceOrder.findAndCountAll(selector)
 
         for (const element of serviceOrders.rows)
             await serviceOrder(element.dataValues)
@@ -144,22 +191,28 @@ const findByCustomerAndDressmakerId = async (req, res) => {
     const dressmakerId = req.params.dressmakerId
     const customerId = req.params.customerId
 
-    const selector = { dressmakerId: dressmakerId, customerId: customerId }
-
     try {
         const orderByPrice = req.query.orderByPrice
+
         const limit = req.query.limit
         const offset = req.query.offset
 
-        let serviceOrders = await models.ServiceOrder.findAndCountAll(
-            {
-                order: orderByPrice ? [['totalPrice', orderByPrice.toUpperCase()]] : [],
-                where: selector,
-                include: ['services'],
-                limit: limit,
-                offset: offset,
-            }
-        )
+        const status = req.query.status
+
+        const dateField = req.query.dateField
+        const startDate = req.query.startDate
+        const endDate = req.query.endDate
+
+        let selector = getSelector(orderByPrice, limit, offset, status, dateField, startDate, endDate)
+        selector.where[Op.and].push({
+            ['customerId']: customerId
+        })
+        selector.where[Op.and].push({
+            ['dressmakerId']: dressmakerId
+        })
+        console.log(selector)
+
+        let serviceOrders = await models.ServiceOrder.findAndCountAll(selector)
 
         for (const element of serviceOrders.rows)
             await serviceOrder(element.dataValues)
@@ -272,8 +325,8 @@ const findByIdAndCustomerAndDressmakerId = async (req, res) => {
 }
 
 const store = async (req, res) => {
-    const { deliveryDate, entryDate, deliveryPeriod, totalPrice, status, customerId, dressmakerId } = req.body
-
+    const { deliveryDate, deliveryPeriod, totalPrice, status, customerId, dressmakerId } = req.body
+    const entryDate = new Date()
     const serviceOrder = { deliveryDate, entryDate, deliveryPeriod, totalPrice, status, customerId, dressmakerId }
 
     const error = validateServiceOrder(res, serviceOrder)
@@ -289,7 +342,7 @@ const store = async (req, res) => {
 }
 
 const update = async (req, res) => {
-    const { id, deliveryDate, entryDate, deliveryPeriod, totalPrice, status, customerId, dressmakerId } = req.body
+    const { id, deliveryDate, deliveryPeriod, totalPrice, status, customerId, dressmakerId } = req.body
     const idRequest = req.params.id
 
     console.log(idRequest, id)
@@ -297,7 +350,7 @@ const update = async (req, res) => {
     if (parseInt(idRequest) !== id)
         return Exception(res, 400, 'Path ID and payload ID does not matches')
 
-    const service = { id, deliveryDate, entryDate, deliveryPeriod, totalPrice, status, customerId, dressmakerId }
+    const service = { id, deliveryDate, deliveryPeriod, totalPrice, status, customerId, dressmakerId }
 
     if (!await models.ServiceOrder.findOne({ where: { id: id } }))
         return Exception(res, 404, 'Service Order not found')
