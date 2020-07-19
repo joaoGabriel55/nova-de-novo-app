622cbe9141bff3ef63c7147e316eb1fea8f20a6d
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2020-07-18 23:11:10 -0300

    Removed console.logs AuthService.js

--- front-end/nova-de-novo/src/services/auth/AuthService.js
+++ front-end/nova-de-novo/src/services/auth/AuthService.js
@@ -5,10 +5,8 @@ dotenv.config()
 
 export const userLogin = (userCrendentials) => {
     const secret = process.env.REACT_APP_TOKEN_SECRET
-    console.log(userCrendentials)
     userCrendentials.password = jwt.sign(
         { passwordHash: userCrendentials.password }, secret, { expiresIn: '120s' }
     )
-    console.log(userCrendentials)
     return API.post('/auth/login', userCrendentials)
 }
